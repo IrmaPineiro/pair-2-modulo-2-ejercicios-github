b3e9cc8f0838b95b7935a069976b13cb07c3549e
LauM25 <laurammwork93@gmail.com>
2025-03-05 11:26:00 +0100

    change the face

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 "use strict"
-
+/*
+Seleccionar la usuaria va a hacer click
+Cuando la usuaria haga click
+    se cambie la cara(contenido) y sea un guiño
+*/
+const head = document.querySelector(".js-head")
+const face = document.querySelector(".js-face")
+function handleClick(event) {
+    event.preventDefault();
+    face.innerHTML = ";)"
+}
+head.addEventListener("click", handleClick);
